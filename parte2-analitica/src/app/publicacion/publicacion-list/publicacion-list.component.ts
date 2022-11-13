4937977743d6f109a760116ca855303a93f691ac
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-13 13:36:27 -0500

    Delete comments of getpublicaciones

--- parte2-analitica/src/app/publicacion/publicacion-list/publicacion-list.component.ts
+++ parte2-analitica/src/app/publicacion/publicacion-list/publicacion-list.component.ts
@@ -23,14 +23,6 @@ export class PublicacionListComponent implements OnInit {
     this.publicacionService.getPublicaciones().subscribe(
       (publicaciones) => this.publicaciones = publicaciones
     );
-    /*
-    this.publicacionService.getPublicaciones()
-    this.publicaciones.push(new Publicacion(1, 'El suicidio ta feo, pls help me aa', '2020-01-01 11:20:00', 'suicide', 'TrashCam9'))
-    this.publicaciones.push(new Publicacion(2, 'El suicidio ta feo, pls help me ai', '2020-01-01 11:20:00', 'suicide', 'TrashCam9'))
-    this.publicaciones.push(new Publicacion(3, 'El suicidio ta feo, pls help me ai', '2020-01-01 11:20:00', 'suicide', 'TrashCam9'))
-    this.publicaciones.push(new Publicacion(4, 'El suicidio ta feo, pls help me ai', '2020-01-01 11:20:00', 'suicide', 'TrashCam9'))
-    this.publicaciones.push(new Publicacion(5, 'El suicidio ta feo, pls help me ai', '2020-01-01 11:20:00', 'suicide', 'TrashCam9'))
-    this.publicaciones.push(new Publicacion(6, 'El suicidio ta feo, pls help me ai', '2020-01-01 11:20:00', 'suicide', 'TrashCam9'))*/
   }
 
 }
