4bc438d97b183e351c743c277f23e93201e90ffa
WilliamMendez <69637352+WilliamMendez@users.noreply.github.com>
2022-11-12 20:32:19 -0500

    api

---
+++ API/main.py
@@ -0,0 +1,34 @@
+# Correr los siguientes comandos:
+#   pip install "uvicorn[standard]"
+#   pip install fastapi
+#   pip install pydantic
+
+# Para correr el servidor:
+#   uvicorn main:app --reload
+
+from typing import List
+
+import pandas as pd
+from fastapi import FastAPI
+
+from DataModel import DataModel, DataModelComplete
+from PredictionModel import PredictionModel
+
+app = FastAPI()
+
+@app.on_event("startup")
+async def startup_event():
+    global prediction_model
+    prediction_model = PredictionModel()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.post("/predict")
+def make_prediction(dataModel: List[DataModel]):
+    df = pd.DataFrame([x.dict() for x in dataModel])
+    df.columns = dataModel[0].columns()
+    results = prediction_model.make_prediction(df)
+    return results.tolist()
