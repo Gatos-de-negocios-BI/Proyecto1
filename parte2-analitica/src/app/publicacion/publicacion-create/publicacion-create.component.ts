185ea4ad683e6a55ffc2293dda50cec435998a17
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-13 11:11:57 -0500

    Create publicacion-create component

---
+++ parte2-analitica/src/app/publicacion/publicacion-create/publicacion-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-publicacion-create',
+  templateUrl: './publicacion-create.component.html',
+  styleUrls: ['./publicacion-create.component.css']
+})
+export class PublicacionCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
