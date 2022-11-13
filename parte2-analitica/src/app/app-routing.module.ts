265dd9eafb8f8e1a86b6fdac4f5d5962fed4e044
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-13 11:15:45 -0500

    Add routing for publicar component

--- parte2-analitica/src/app/app-routing.module.ts
+++ parte2-analitica/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PublicacionListComponent } from './publicacion/publicacion-list/publicacion-list.component';
+import { PublicacionCreateComponent } from './publicacion/publicacion-create/publicacion-create.component';
 
 const routes: Routes = [
-  { path: '', component: PublicacionListComponent}
+  { path: '', component: PublicacionListComponent},
+  { path: 'posts/create', component: PublicacionCreateComponent}
 ];
 
 @NgModule({
--- parte2-analitica/src/app/publicacion/publicacion-routing.module.ts
+++ parte2-analitica/src/app/publicacion/publicacion-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PublicacionListComponent } from './publicacion-list/publicacion-list.component';
+import { PublicacionCreateComponent } from './publicacion-create/publicacion-create.component';
 
 const routes: Routes = [{
   path: 'posts',
@@ -8,6 +9,10 @@ const routes: Routes = [{
     {
       path: 'list',
       component: PublicacionListComponent
+    },
+    {
+      path: 'create',
+      component: PublicacionCreateComponent
     }
   ]
 }];
