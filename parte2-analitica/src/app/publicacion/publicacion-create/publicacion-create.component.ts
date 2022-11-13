ad30e0ed0c04cb7e5579f0da5cdc9ead57d52a9c
Camila Gómez <mc.gomezh1@uniandes.edu.co>
2022-11-13 15:37:47 -0500

    Commit dudoso

--- parte2-analitica/src/app/publicacion/publicacion-create/publicacion-create.component.ts
+++ parte2-analitica/src/app/publicacion/publicacion-create/publicacion-create.component.ts
@@ -1,22 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { PublicacionService } from '../publicacion.service';
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-publicacion-create',
   templateUrl: './publicacion-create.component.html',
-  styleUrls: ['./publicacion-create.component.css']
+  styleUrls: ['./publicacion-create.component.css'],
+  providers: [NgbModalConfig, NgbModal],
 })
 export class PublicacionCreateComponent implements OnInit {
 
   prediction: string = 'non-suicide';
-  constructor(private publicacionService: PublicacionService) {  }
+  constructor(private publicacionService: PublicacionService, config: NgbModalConfig, private modalService: NgbModal) {
+    config.backdrop = 'static';
+    config.keyboard = false;
+  }
 
   ngOnInit() {
   }
 
-  getPrediction(text: string): void {
+  getPrediction(text: string, content:any): void {
     this.publicacionService.predictText(text).subscribe((prediction) => {
       this.prediction = prediction;
+      if (prediction == 'suicide'){
+        this.modalService.open(content)
+      }
       console.log(prediction);
     });
   }
